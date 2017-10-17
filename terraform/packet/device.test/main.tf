681d7d74f4a4994ad2c63cf8964cee722215489c
l5x <189198+l5x@users.noreply.github.com>
2017-10-17 20:27:49 +0000

    add: terraform/packet/device

---
+++ terraform/packet/device.test/main.tf
@@ -0,0 +1,28 @@
+provider "packet" {}
+
+variable "PACKET_PROJECT_ID" {}
+
+variable "facilities" {
+  default = ["ams1", "sjc1"]
+}
+
+variable "plans" {
+  default = ["baremetal_0", "baremetal_0"]
+}
+
+variable "volume_plans" {
+  default = ["storage_2", "storage_1"]
+}
+
+variable "volume_sizes" {
+  default = [60, 60]
+}
+
+module "packer_device" {
+  source       = "../device"
+  project_id   = "${var.PACKET_PROJECT_ID}"
+  facilities   = "${var.facilities}"
+  plans        = "${var.plans}"
+  volume_plans = "${var.volume_plans}"
+  volume_sizes = "${var.volume_sizes}"
+}
---
+++ terraform/packet/device/main.tf
@@ -0,0 +1,101 @@
+variable "project_id" {
+  description = "The id of the project in which to create the device(s)"
+}
+
+variable "facilities" {
+  type        = "list"
+  description = "List of the device facilities"
+}
+
+variable "plans" {
+  description = "List of the device hardware config slugs"
+  type        = "list"
+}
+
+variable "volume_plans" {
+  type        = "list"
+  description = "List of the volume service plan slugs"
+}
+
+variable "volume_sizes" {
+  type        = "list"
+  description = "List of the volume sizes in GB"
+}
+
+variable "snapshot_frequency" {
+  description = "The snapshot frequency"
+  default = "1month"
+}
+
+variable "snapshot_count" {
+  description = "Keep n recent snapshots"
+  default = "3"
+}
+
+resource "random_pet" "name" {
+  count = "${length(var.facilities)}"
+}
+
+resource "packet_reserved_ip_block" "eip" {
+  count      = "${length(var.facilities)}"
+  project_id = "${var.project_id}"
+  facility   = "${element(var.facilities, count.index)}"
+  quantity   = 2
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "packet_device" "device" {
+  count            = "${length(var.facilities)}"
+  project_id       = "${var.project_id}"
+  hostname         = "${element(random_pet.name.*.id, count.index)}"
+  facility         = "${element(var.facilities, count.index)}"
+  plan             = "${element(var.plans, count.index)}"
+  billing_cycle    = "hourly"
+  operating_system = "centos_7"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "packet_ip_attachment" "attachment" {
+  count         = "${length(var.facilities)}"
+  device_id     = "${element(packet_device.device.*.id, count.index)}"
+  cidr_notation = "${cidrhost(element(packet_reserved_ip_block.eip.*.cidr_notation, count.index), 0)}/32}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "packet_volume" "volume" {
+  billing_cycle = "hourly"
+  count         = "${length(var.facilities)}"
+  description   = "${element(random_pet.name.*.id, count.index)} volume"
+  facility      = "${element(var.facilities, count.index)}"
+  plan          = "${element(var.volume_plans, count.index)}"
+  project_id    = "${var.project_id}"
+  size          = "${element(var.volume_sizes, count.index)}"
+
+  snapshot_policies = {
+    snapshot_frequency = "${var.snapshot_frequency}"
+    snapshot_count     = "${var.snapshot_count}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "packet_volume_attachment" "attachment" {
+  count     = "${length(var.facilities)}"
+  device_id = "${element(packet_device.device.*.id, count.index)}"
+  volume_id = "${element(packet_volume.volume.*.id, count.index)}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
