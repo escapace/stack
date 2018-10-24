1b476225011da2315c840b700206d99d83564d95
l5x <189198+l5x@users.noreply.github.com>
2018-10-24 16:37:01 +0400

    feat: upgrade to ansible 2.7.0

--- terraform/packet/device.test/main.tf
+++ terraform/packet/device.test/main.tf
@@ -3,19 +3,19 @@ provider "packet" {}
 variable "PACKET_PROJECT_ID" {}
 
 variable "facilities" {
-  default = ["ams1", "ams1"]
+  default = ["ams1"]
 }
 
 variable "plans" {
-  default = ["baremetal_0", "baremetal_0"]
+  default = ["baremetal_0"]
 }
 
 variable "volume_plans" {
-  default = ["storage_2", "storage_1"]
+  default = ["storage_2"]
 }
 
 variable "volume_sizes" {
-  default = [60, 60]
+  default = [120]
 }
 
 module "packer_device" {
